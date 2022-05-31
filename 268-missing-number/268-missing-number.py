35ddb32a46cbfacb5a8e6e144ad1d1e88f44a712
Mike Tillman <59715611+miketillman95@users.noreply.github.com>
2022-05-31 13:09:10 +0100

    Time: 312 ms (15.83%), Space: 15.4 MB (15.52%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,11 +1,14 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        if (len(nums) ==0):
+            return 0
         
+        nums.sort()
         for n in range(len(nums)):
             print(range(len(nums)))
-            nums.sort()
-            print(nums.sort())
+            
             if n!=nums[n]:
                 return n
+            
         return len(nums)
             
