c2a422b4a349c252da242f925f50c070338e7784
Mike Tillman <59715611+miketillman95@users.noreply.github.com>
2022-07-18 10:50:12 +0100

    Time: 48 ms (45.02%), Space: 14 MB (5.19%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.py
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -3,8 +3,7 @@ class Solution:
         s_last = s[-1]
         print(s_last)
         
-        for i in s:
-            print(i)
-            if s_last:
-                return len(s.split()[-1])
-                
+        # for i in s:
+        #     print(i)
+        if s_last:
+            return len(s.split()[-1])
